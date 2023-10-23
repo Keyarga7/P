ab6a406b39320df215e9f42adfd990fc02943cc3
Keyarga7 <131921725+Keyarga7@users.noreply.github.com>
2023-10-23 10:43:48 -0600

    Update config.js

--- config.js
+++ config.js
@@ -10,9 +10,9 @@ import moment from 'moment-timezone';
 global.botnumber = ""
 
 global.owner = [
-  ['5219992095479', '👑 Mystic - Creador 👑', true],
-  ['50499698072', '💫 Mystic - Collaborator 1 💫', true],
-  ['51940617554', '💫 Mystic - Collaborator 2 💫', true],
+  ['5215647084779', '👑 Mystic - Creador 👑', true],
+  ['5212381697271', '💫 Mystic - Collaborator 1 💫', true],
+  ['5493743594699', '💫 Mystic - Collaborator 2 💫', true],
   ['51996089079', '💫 Mystic - Collaborator 3 💫', true],
   ['5218442286089', '💫 Mystic - Collaborator 4 💫', true],
   ['50246028932', '💫 Mystic - Collaborator 5 💫', true],
